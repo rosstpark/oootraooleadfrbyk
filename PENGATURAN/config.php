6023c0c63dfcdd7fd302686ac2f705b8ed358c16
brandoncharliem <50611298+brandoncharliem@users.noreply.github.com>
2019-08-08 20:35:13 +0700

    Add files via upload

---
+++ PENGATURAN/config.php
@@ -0,0 +1,32 @@
+<?php
+error_reporting(0);
+$ssl			= 'https'; // jika mau pakai ssl/https rubah valuenya disini
+$domain			= ''.$ssl.'://'.$_SERVER['HTTP_HOST'];
+$domen 			= $_SERVER['HTTP_HOST'];
+$lpdomain		= '';
+$agclist		= 'pop';
+$enginepdf		= 'mpdf'; // engine yang tersedia saat ini dompdpf,mpdf dan fpdf
+$Premium		= ''; // Ganti Offer disini
+$Biasa			= ''; // Ganti Offer disini
+$niche			= 'jagoo'; //ini adalah nama folder sitemap
+$site_name		= 'Ebook and Manual Reference'; // Ganti Nama Web anda Agar Lebih Unik
+$site_description	= 'Ebook ,ePub and Manual Book Reviews';  // Ganti Deskripsi Web anda Agar Lebih Unik
+$site_keyword		= 'Free Ebook Download,Download Ebook Free,Free PDF Books'; // Ganti Keyword Web anda Agar Lebih Unik
+$fformat		= 'pdf';
+$negara			= 'com';
+$feed			= '5000'; //Untuk Menampilkan Berapa Url di feed (Google Menyarankan Maksimal 500 Post didalam Feed)
+$linkstyle		= '-'; // untuk merubah style url pakai strip - atau underscore _ atau apapun bebas
+$slugpost		= '/book'; //Untuk Merubah Slug keyword + Slash  misalkan ebook/
+$slugpost2		= '/search'; //Untuk Merubah Slug keyword + Slash  misalkan ebook/
+$linkfont		= 'ucwords'; // untuk mengubah font permalink yg tersedia strtolower,ucwords dan strtoupper
+$sizepdf		= 'custom';  //ukuran yang tersedia halaman pdf (letter,legal,ledger,tabloid,executive,folio)
+$paperstyle             = 'portrait'; // Style yg tersedia dihalaman pdf hanya (portrait dan landscape)
+$lang			= 'en-US';
+$imgna			= '/na.jpg';
+$fontpdf		= '{Agency FB|Albertina|Antiqua|Architect|Arial|BankFuturistic|BankGothic|Blackletter|Blagovest|Calibri|Comic Sans MS|Courier|Cursive|Decorative|Fantasy|Fraktur|Frosty|Garamond|Georgia|Helvetica|Impact|Minion|Modern|Monospace|Open Sans|Palatino|Perpetua|Roman|Sans-serif|Serif|Script|Swiss|Times|Times New Roman|Tw Cen MT|Verdana}';
+$spinjudul		= '{Download Here:|Free Download:|Download:|Download Now:|Download Free:}';
+$spinjudul2		= '{Read Online|Online Reading|Read E-Book Online|Free Reading|Reading Free}';
+$singledesc = '%booktitle% is {the best|big|most popular} ebook you {want|must read|need}. You can {get|download|read} any ebooks you wanted like %booktitle% in {simple|easy} step and you can {download|get|save|read full version} it now.||{The big|The most popular|Popular|Best|Great|Nice} ebook you {should|must|want to} read is %booktitle%. {I am|We are} {sure|promise} you will {love|like} the %booktitle%. You can download it to your {computer|laptop|smartphone} {with|in|through} {simple|easy|light} steps.||Are you {looking for|search|trying to find} %booktitle%? {Then you|Then you definitely|You then|Then you certainly} come {to the right|right|off to the right|to the correct} place {to get the|to obtain the|to find the|to have the} %booktitle%. {You can read|Read|Look for|Search for} any ebook online with {easy steps|simple steps|basic steps|simple actions}. But if you want to {save|download|get} it to your {computer|laptop|smartphone}, you can download {much|more} of ebooks now.||Download this {big|most popular|popular|best|great|nice} ebook and read the %booktitle% ebook. {You will not|You won\'t|You\'ll not|You can\'t} find this ebook anywhere online. {Read the|Browse the|See the|Look at} any books now and {if you do not|if you don\'t|unless you|should you not} have {a lot of time|considerable time|lots of time|time and effort} {to read|to see|to learn|you just read}, {you can|you are able to|it is possible to|you\'ll be able to} download any ebooks {to your|for your|in your|on your} {computer|laptop|smartphone|device} and {read|check} later.';
+$singledesc2 = '{We all know|Everyone knows|Everybody knows|You know} that reading %booktitle%  {is beneficial|is helpful|is effective|is useful}, because {we can|we are able to|we could|we can easily} get {a lot of information|enough detailed information online|information|too much info online} {from the|in the|from your|through the} {reading materials|resources}. {Technology has|Technologies have} developed, and reading %booktitle%  books {can be|could be|may be|might be} {more convenient|easier|far more convenient|far easier} {and easier|and simpler|and much easier}. {We can|We are able to|We could|We can easily} read books {on our|on the} mobile, tablets and Kindle, etc. Hence, {there are many|there are lots of|there are numerous|there are several} books {coming into|entering|getting into|being received by} PDF format. {Below are some|Listed below are some|Right here|Several} websites for downloading free PDF books {where you can|where one can|to|which you could} acquire {as much|just as much|the maximum amount of|all the} knowledge {as you want|as you would like|as you wish|as you desire}.';
+$email = 'emaildmca@gmail.com'; //Ganti Dengan Email Anda yang Mudah dihubungi bila ada complain DMCA 
+?>
